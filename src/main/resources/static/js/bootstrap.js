61ed043e814c2f7276857bbbd4c382e365c8d3c5
marins <tiagomarinsq97@gmail.com>
2018-09-30 00:03:54 -0300

    alterações no HTML
    Arrumei o alinhamento e coloquei mascaras nos campos, mas ainda não em todos xD, mudei as cores tbm

--- src/main/resources/static/js/bootstrap.js
+++ src/main/resources/static/js/bootstrap.js
@@ -22,6 +22,8 @@ function _defineProperties(target, props) {
   }
 }
 
+
+
 function _createClass(Constructor, protoProps, staticProps) {
   if (protoProps) _defineProperties(Constructor.prototype, protoProps);
   if (staticProps) _defineProperties(Constructor, staticProps);
