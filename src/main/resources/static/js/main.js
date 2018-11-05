61f744666cb8c3155b61cd8ca5a31469ffdf1e88
marins <tiagomarinsq97@gmail.com>
2018-11-05 00:48:53 -0200

    modal para cadastro de ponto de pauta funcionando

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -10,3 +10,19 @@ $(document).ready(function(){
 		$('#myModal').modal();
 	});
 });
+
+$(document).ready(function(){
+	$('.container .nBtn').on('click', function(event){
+		event.preventDefault();
+		var href = $(this).attr('href');
+			$.get(href, function(pontoPauta, status){
+				$('.myForm #ordem').val(pontoPauta.ordem);
+				$('.myForm #descricao').val(pontoPauta.descricao);
+				$('.myForm #tempo').val(pontoPauta.tempo);
+				$('.myForm #discussao').val(pontoPauta.discussao);
+				$('.myForm #responsavel').val(pontoPauta.responsavel);
+				$('.myForm #tipo').val(pontoPauta.tipo);
+			});
+			$('.myForm #exampleModal').modal();
+	});
+});
