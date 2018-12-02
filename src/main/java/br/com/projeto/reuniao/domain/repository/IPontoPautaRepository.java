1b6fe1d2bd86700764bd913a05f9a49e06aecf28
Pablo <Pablo@DESKTOP-4SKP85L.local>
2018-12-02 14:05:22 -0200

    Ponto da pauta

--- src/main/java/br/com/projeto/reuniao/domain/repository/IPontoPautaRepository.java
+++ src/main/java/br/com/projeto/reuniao/domain/repository/IPontoPautaRepository.java
@@ -1,6 +1,10 @@
 package br.com.projeto.reuniao.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.projeto.reuniao.domain.entity.PontoPauta;
@@ -8,4 +12,10 @@ import br.com.projeto.reuniao.domain.entity.PontoPauta;
 @Repository
 public interface IPontoPautaRepository extends JpaRepository<PontoPauta, Long>{
 
+	@Query("SELECT new PontoPauta(pontoPauta.id, pontoPauta.ordem, pontoPauta.descricao, pontoPauta.tempo, pontoPauta.discussao, "
+			+ "pontoPauta.responsavel, pontoPauta.tipo, pontoPauta.reuniao) "
+			+ "FROM PontoPauta pontoPauta "
+			+ "WHERE pontoPauta.reuniao.id = :idReuniao")
+	List<PontoPauta> listByReuniaoId(@Param("idReuniao") Long idReuniao);
+	
 }
