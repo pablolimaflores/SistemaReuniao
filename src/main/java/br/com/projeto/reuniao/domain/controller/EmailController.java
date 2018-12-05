54814c83c6daa33fbda26c69a1c9b804f8351424
Pablo <Pablo@192.168.41.167>
2018-12-05 03:26:02 -0200

    Email

--- src/main/java/br/com/projeto/reuniao/domain/controller/EmailController.java
+++ src/main/java/br/com/projeto/reuniao/domain/controller/EmailController.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.projeto.reuniao.domain.entity.Participante;
+import br.com.projeto.reuniao.domain.entity.PontoPauta;
 import br.com.projeto.reuniao.domain.entity.Reuniao;
 import br.com.projeto.reuniao.domain.service.ParticipanteService;
 import br.com.projeto.reuniao.domain.service.PessoaService;
+import br.com.projeto.reuniao.domain.service.PontoPautaService;
 import br.com.projeto.reuniao.domain.service.ReuniaoService;
 
 @Controller
@@ -33,6 +35,9 @@ public class EmailController {
 	
 	@Autowired
 	private PessoaService pessoaService;
+	
+	@Autowired
+	private PontoPautaService pontoPautaService;
 
 
 	@GetMapping(path = "reunioes/reunioesEdit/{id}/email")
@@ -46,6 +51,8 @@ public class EmailController {
 
 			ArrayList<Participante> participantes = (ArrayList<Participante>) this.participanteService
 					.listParticipanteByReuniaoId(id);
+			
+			ArrayList<PontoPauta> pontosPauta = (ArrayList<PontoPauta>) this.pontoPautaService.listPontoPautaByReuniaoId(id);					
 
 			participantes.stream().forEach(p -> {
 				try {
@@ -58,18 +65,41 @@ public class EmailController {
 					
 					StringBuilder emailHtml = new StringBuilder("");
 					
+					emailHtml.append("<p>Olá.</p>")
+					.append("<p>Você está recebendo este e-mail é para informar os dados desta reunião agendada.</p>");
+					
+					//Dados gerais					
 					emailHtml.append("<h2>" + reuniao.getTitulo() + "</h2>")														
 					.append("<p><strong>Tipo:</strong> " + reuniao.getTipo().getNome() + "</p>")
 					.append("<p><strong>Local:</strong> " + reuniao.getLocal() + "</p>")
 					.append("<p><strong>Data:</strong> " + reuniao.getData().format(formatter) + "</p>")
 					.append("<p><strong>Horario:</strong> das " + reuniao.getHoraInicio() + " até às " + reuniao.getHoraFim() + "</p>")
-					.append("<p><strong>Objetivo:</strong> " + reuniao.getObjetivo() + "</p>");
-					if (!reuniao.getPreRequisito().isEmpty()) {
-						emailHtml.append("<p><strong>Pré-requisito:</strong> " + reuniao.getPreRequisito() + "</p>");
-					}
+					.append("<p><strong>Objetivo:</strong> " + reuniao.getObjetivo() + "</p>")
+					.append(!reuniao.getPreRequisito().isEmpty() ? "" : "<p><strong>Pré-requisito:</strong> " + reuniao.getPreRequisito() + "</p>");				
+					
+					//Participantes
+					emailHtml.append("<h4>Participantes</h4>")
+					.append("<ul>");
+					
+					participantes.stream().forEach(partic -> {						
+						emailHtml.append("<li><strong>Nome:</strong> " + partic.getPessoa().getNome() + "</li>");
+					});			
+					
+					emailHtml.append("</ul>");
+					
+					//Pontos de pauta
+					emailHtml.append("<h4>Pontos de Pauta</h4>");					
+					
+					pontosPauta.stream().forEach(pon -> {						
+						emailHtml.append("<p><strong>Pauta:</strong> " + pon.getDescricao() + "</p>")
+						.append("<p><strong>Tipo:</strong> " + pon.getTipo().getNome() + "</p>")
+						.append("<p><strong>Responsável:</strong> " + pon.getResponsavel().getNome() + "</p>")
+						;					
+					});
 
 					helper.setText(emailHtml.toString(), true);
 					
+					//Enviar e-mail
 					mailSender.send(mail);
 					System.out.println("******** E-mail enviado para o participante " + p.getPessoa().getNome() + " ********");
 				} catch (Exception e) {
